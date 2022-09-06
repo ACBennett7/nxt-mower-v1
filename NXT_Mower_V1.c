e6c8d9b33c67567e4f5114aaf12e21ec8f2f42a1
Andrew Bennett <88630714+ACBennett7@users.noreply.github.com>
2022-09-05 21:11:00 -0400

    Update values for physical robot instead of virtual sim

--- NXT_Mower_V1.c
+++ NXT_Mower_V1.c
@@ -4,38 +4,38 @@ task main()
 {
 	int turns = 1;
 	while(true){
-		if(SensorValue[sonarSensor] > 30)
+		if(SensorValue[sonarSensor] > 20)
 		{
 			motor[motorB] = 75;
 			motor[motorC] = 75;
-		} else {
-				if(turns % 2 == 0){
-					motor[motorB] = 50;
-					motor[motorC] = -50;
-					wait(.75);
-
-					motor[motorB] = 75;
-					motor[motorC] = 75;
-					wait(.95);
-
-					motor[motorB] = 50;
-					motor[motorC] = -50;
-					wait(.77);
-
-					turns++;
 			} else {
-					motor[motorB] = -50;
-					motor[motorC] = 50;
-					wait(.75);
+			if(turns % 2 == 0){
+				motor[motorB] = 50;
+				motor[motorC] = -50;
+				wait(1.2);
 
-					motor[motorB] = 75;
-					motor[motorC] = 75;
-					wait(.95);
+				motor[motorB] = 75;
+				motor[motorC] = 75;
+				wait(1.25);
 
-					motor[motorB] = -50;
-					motor[motorC] = 50;
-					wait(.75);
-					turns++;
+				motor[motorB] = 50;
+				motor[motorC] = -50;
+				wait(1.2);
+
+				turns++;
+				} else {
+				motor[motorB] = -50;
+				motor[motorC] = 50;
+				wait(1.2);
+
+				motor[motorB] = 75;
+				motor[motorC] = 75;
+				wait(1.25);
+
+				motor[motorB] = -50;
+				motor[motorC] = 50;
+				wait(1.2);
+				turns++;
 			}
 		}
 	}
